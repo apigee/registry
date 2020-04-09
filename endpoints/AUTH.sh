b8520997bd590c8487a70367f98d6450aeab27e3
Tim Burks <timburks@google.com>
2020-04-08 17:36:23 -0700

    use gcloud command to get user email for endpoints auth

--- endpoints/AUTH.sh
+++ endpoints/AUTH.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#gcloud auth login
+# The following assumes you have run `gcloud auth login`.
 
-export CLI_FLAME_CLIENT_EMAIL="timburks@google.com"
+export CLI_FLAME_CLIENT_EMAIL=$(gcloud config list account --format "value(core.account)")
 
 export CLI_FLAME_AUDIENCES=$(gcloud beta run services describe flame --platform managed --format="value(status.address.url)")
 
 export CLI_FLAME_ADDRESS=${CLI_FLAME_AUDIENCES#https://}:443
-
-#export CLI_FLAME_TOKEN=$(gcloud auth print-identity-token ${CLI_FLAME_CLIENT_EMAIL} --audiences="$CLI_FLAME_AUDIENCES")
 
 export CLI_FLAME_TOKEN=$(gcloud auth print-identity-token ${CLI_FLAME_CLIENT_EMAIL})
