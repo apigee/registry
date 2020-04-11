ddf3f655804cabcd42b8b6255c9ed4d784276813
Tim Burks <timburks@google.com>
2020-04-10 22:02:37 -0700

    reorganized flame list implementation

--- cmd/flame/cmd/list.go
+++ cmd/flame/cmd/list.go
@@ -6,162 +6,220 @@ import (
 	"log"
 
 	"apigov.dev/flame/cmd/flame/connection"
+	"apigov.dev/flame/gapic"
 	"apigov.dev/flame/models"
 	rpc "apigov.dev/flame/rpc"
 	"github.com/spf13/cobra"
 	"google.golang.org/api/iterator"
+	"google.golang.org/grpc/status"
 )
 
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Args: cobra.MinimumNArgs(1),
+	Short: "List resources in the API model.",
+	Long:  "List resources in the API model.",
+	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("list called with %+v\n", args)
+		client, err := connection.NewClient()
+		if err != nil {
+			log.Fatalf("%s", err.Error())
+		}
+		ctx := context.TODO()
+
 		name := args[0]
 		if m := models.ProductsRegexp().FindAllStringSubmatch(name, -1); m != nil {
-			client, err := connection.NewClient()
-			if err != nil {
-				log.Fatalf("%s", err.Error())
-			}
-			ctx := context.TODO()
-			request := &rpc.ListProductsRequest{
-				Parent: "projects/" + m[0][1],
-			}
-			it := client.ListProducts(ctx, request)
-			for {
-				product, err := it.Next()
-				if err == iterator.Done {
-					break
-				} else if err != nil {
-					log.Fatalf("%s", err.Error())
-				}
-				fmt.Println(product.Name)
-			}
+			err = listProducts(ctx, client, m[0])
 		} else if m := models.VersionsRegexp().FindAllStringSubmatch(name, -1); m != nil {
-			client, err := connection.NewClient()
-			if err != nil {
-				log.Fatalf("%s", err.Error())
-			}
-			ctx := context.TODO()
-			request := &rpc.ListVersionsRequest{
-				Parent: "projects/" + m[0][1] + "/products/" + m[0][2],
-			}
-			it := client.ListVersions(ctx, request)
-			for {
-				version, err := it.Next()
-				if err == iterator.Done {
-					break
-				} else if err != nil {
-					log.Fatalf("%s", err.Error())
-				}
-				fmt.Println(version.Name)
-			}
+			err = listVersions(ctx, client, m[0])
 		} else if m := models.SpecsRegexp().FindAllStringSubmatch(name, -1); m != nil {
-			client, err := connection.NewClient()
-			if err != nil {
-				log.Fatalf("%s", err.Error())
-			}
-			ctx := context.TODO()
-			request := &rpc.ListSpecsRequest{
-				Parent: "projects/" + m[0][1] + "/products/" + m[0][2] + "/versions/" + m[0][3],
-			}
-			it := client.ListSpecs(ctx, request)
-			for {
-				spec, err := it.Next()
-				if err == iterator.Done {
-					break
-				} else if err != nil {
-					log.Fatalf("%s", err.Error())
-				}
-				fmt.Println(spec.Name)
-			}
+			err = listVersions(ctx, client, m[0])
 		} else if m := models.PropertiesRegexp().FindAllStringSubmatch(name, -1); m != nil {
-			client, err := connection.NewClient()
-			if err != nil {
-				log.Fatalf("%s", err.Error())
-			}
-			ctx := context.TODO()
-			request := &rpc.ListPropertiesRequest{
-				Parent: "projects/" + m[0][1],
-			}
-			it := client.ListProperties(ctx, request)
-			for {
-				property, err := it.Next()
-				if err == iterator.Done {
-					break
-				} else if err != nil {
-					log.Fatalf("%s", err.Error())
-				}
-				fmt.Println(property.Name)
-			}
+			err = listProperties(ctx, client, m[0])
 		} else if m := models.ProductRegexp().FindAllStringSubmatch(name, -1); m != nil {
-			client, err := connection.NewClient()
-			if err != nil {
-				log.Fatalf("%s", err.Error())
+			segments := m[0]
+			if sliceContainsString(segments, "-") {
+				err = listProducts(ctx, client, segments)
+			} else {
+				err = getProduct(ctx, client, segments)
 			}
-			ctx := context.TODO()
-			request := &rpc.GetProductRequest{
-				Name: "projects/" + m[0][1] + "/products/" + m[0][2],
-			}
-			product, err := client.GetProduct(ctx, request)
-			fmt.Printf("%+v\n", product)
 		} else if m := models.VersionRegexp().FindAllStringSubmatch(name, -1); m != nil {
-			client, err := connection.NewClient()
-			if err != nil {
-				log.Fatalf("%s", err.Error())
+			segments := m[0]
+			if sliceContainsString(segments, "-") {
+				err = listVersions(ctx, client, segments)
+			} else {
+				err = getVersion(ctx, client, segments)
 			}
-			ctx := context.TODO()
-			request := &rpc.GetVersionRequest{
-				Name: "projects/" + m[0][1] + "/products/" + m[0][2] + "/versions/" + m[0][3],
-			}
-			version, err := client.GetVersion(ctx, request)
-			fmt.Printf("%+v\n", version)
 		} else if m := models.SpecRegexp().FindAllStringSubmatch(name, -1); m != nil {
-			client, err := connection.NewClient()
-			if err != nil {
-				log.Fatalf("%s", err.Error())
+			segments := m[0]
+			if sliceContainsString(segments, "-") {
+				err = listSpecs(ctx, client, segments)
+			} else {
+				err = getSpec(ctx, client, segments)
 			}
-			ctx := context.TODO()
-			request := &rpc.GetSpecRequest{
-				Name: "projects/" + m[0][1] + "/products/" + m[0][2] + "/versions/" + m[0][3] + "/specs/" + m[0][4],
-			}
-			spec, err := client.GetSpec(ctx, request)
-			fmt.Printf("%+v\n", spec)
 		} else if m := models.PropertyRegexp().FindAllStringSubmatch(name, -1); m != nil {
-			fmt.Printf("FILE\n")
-			client, err := connection.NewClient()
-			if err != nil {
-				log.Fatalf("%s", err.Error())
+			segments := m[0]
+			if sliceContainsString(segments, "-") {
+				err = listProperties(ctx, client, segments)
+			} else {
+				err = getProperty(ctx, client, segments)
 			}
-			ctx := context.TODO()
-			request := &rpc.GetPropertyRequest{
-				Name: "projects/" + m[0][1] + "/properties/" + m[0][2],
+		} else {
+			fmt.Printf("unsupported argument(s): %+v\n", args)
+		}
+		if err != nil {
+			st, ok := status.FromError(err)
+			if !ok {
+				log.Printf("%s", err.Error())
+			} else {
+				log.Printf("%s", st.Message())
 			}
-			log.Printf("request %+v", request)
-			property, err := client.GetProperty(ctx, request)
-			fmt.Printf("%+v\n", property)
 		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+}
 
-	// Here you will define your flags and configuration settings.
+func listProducts(ctx context.Context,
+	client *gapic.FlameClient,
+	segments []string) error {
+	request := &rpc.ListProductsRequest{
+		Parent: "projects/" + segments[1],
+	}
+	it := client.ListProducts(ctx, request)
+	for {
+		product, err := it.Next()
+		if err == iterator.Done {
+			break
+		} else if err != nil {
+			return err
+		}
+		fmt.Println(product.Name)
+	}
+	return nil
+}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
+func listVersions(ctx context.Context,
+	client *gapic.FlameClient,
+	segments []string) error {
+	request := &rpc.ListVersionsRequest{
+		Parent: "projects/" + segments[1] + "/products/" + segments[2],
+	}
+	it := client.ListVersions(ctx, request)
+	for {
+		version, err := it.Next()
+		if err == iterator.Done {
+			break
+		} else if err != nil {
+			return err
+		}
+		fmt.Println(version.Name)
+	}
+	return nil
+}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func listSpecs(ctx context.Context,
+	client *gapic.FlameClient,
+	segments []string) error {
+	request := &rpc.ListSpecsRequest{
+		Parent: "projects/" + segments[1] + "/products/" + segments[2] + "/versions/" + segments[3],
+	}
+	it := client.ListSpecs(ctx, request)
+	for {
+		spec, err := it.Next()
+		if err == iterator.Done {
+			break
+		} else if err != nil {
+			return err
+		}
+		fmt.Println(spec.Name)
+	}
+	return nil
+}
+
+func listProperties(ctx context.Context,
+	client *gapic.FlameClient,
+	segments []string) error {
+	request := &rpc.ListPropertiesRequest{
+		Parent: "projects/" + segments[1],
+	}
+	it := client.ListProperties(ctx, request)
+	for {
+		property, err := it.Next()
+		if err == iterator.Done {
+			break
+		} else if err != nil {
+			return err
+		}
+		fmt.Println(property.Name)
+	}
+	return nil
+}
+
+func getProduct(ctx context.Context,
+	client *gapic.FlameClient,
+	segments []string) error {
+	request := &rpc.GetProductRequest{
+		Name: "projects/" + segments[1] + "/products/" + segments[2],
+	}
+	product, err := client.GetProduct(ctx, request)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%+v\n", product)
+	return nil
+}
+
+func getVersion(ctx context.Context,
+	client *gapic.FlameClient,
+	segments []string) error {
+	request := &rpc.GetVersionRequest{
+		Name: "projects/" + segments[1] + "/products/" + segments[2] + "/versions/" + segments[3],
+	}
+	version, err := client.GetVersion(ctx, request)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%+v\n", version)
+	return nil
+}
+
+func getSpec(ctx context.Context,
+	client *gapic.FlameClient,
+	segments []string) error {
+	request := &rpc.GetSpecRequest{
+		Name: "projects/" + segments[1] + "/products/" + segments[2] + "/versions/" + segments[3] + "/specs/" + segments[4],
+	}
+	spec, err := client.GetSpec(ctx, request)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%+v\n", spec)
+	return nil
+}
+
+func getProperty(ctx context.Context,
+	client *gapic.FlameClient,
+	segments []string) error {
+	request := &rpc.GetPropertyRequest{
+		Name: "projects/" + segments[1] + "/properties/" + segments[2],
+	}
+	log.Printf("request %+v", request)
+	property, err := client.GetProperty(ctx, request)
+	if err != nil {
+		log.Printf("%+s", err.Error())
+	}
+	fmt.Printf("%+v\n", property)
+	return nil
+}
+
+func sliceContainsString(slice []string, target string) bool {
+	for _, s := range slice {
+		if s == target {
+			return true
+		}
+	}
+	return false
 }
