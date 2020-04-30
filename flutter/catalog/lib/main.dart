455b3714e113f33d1b665d7de55423b5a918b44d
Tim Burks <timburks@google.com>
2020-04-29 21:28:19 -0700

    flutter app with scrolling list

---
+++ flutter/catalog/lib/main.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pagewise/flutter_pagewise.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flame Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.orange,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flame Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 500,
+              child: PagewiseListViewExample(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PagewiseListViewExample extends StatelessWidget {
+  static const int PAGE_SIZE = 10;
+
+  @override
+  Widget build(BuildContext context) {
+    return PagewiseListView(
+        pageSize: PAGE_SIZE,
+        itemBuilder: this._itemBuilder,
+        pageFuture: (pageIndex) =>
+            BackendService.getPosts(pageIndex * PAGE_SIZE, PAGE_SIZE));
+  }
+
+  Widget _itemBuilder(context, PostModel entry, _) {
+    return Column(
+      children: <Widget>[
+        ListTile(
+          leading: Icon(
+            Icons.person,
+            color: Colors.brown[200],
+          ),
+          title: Text(entry.title),
+          subtitle: Text(entry.body),
+        ),
+        Divider()
+      ],
+    );
+  }
+}
+
+class BackendService {
+  static Future<List<PostModel>> getPosts(offset, limit) async {
+    final responseBody = (await http.get(
+            'http://jsonplaceholder.typicode.com/posts?_start=$offset&_limit=$limit'))
+        .body;
+
+    // The response body is an array of items
+    return PostModel.fromJsonList(json.decode(responseBody));
+  }
+
+  static Future<List<ImageModel>> getImages(offset, limit) async {
+    final responseBody = (await http.get(
+            'http://jsonplaceholder.typicode.com/photos?_start=$offset&_limit=$limit'))
+        .body;
+
+    // The response body is an array of items.
+    return ImageModel.fromJsonList(json.decode(responseBody));
+  }
+}
+
+class PostModel {
+  String title;
+  String body;
+
+  PostModel.fromJson(obj) {
+    this.title = obj['title'];
+    this.body = obj['body'];
+  }
+
+  static List<PostModel> fromJsonList(jsonList) {
+    return jsonList.map<PostModel>((obj) => PostModel.fromJson(obj)).toList();
+  }
+}
+
+class ImageModel {
+  String title;
+  String id;
+  String thumbnailUrl;
+
+  ImageModel.fromJson(obj) {
+    this.title = obj['title'];
+    this.id = obj['id'].toString();
+    this.thumbnailUrl = obj['thumbnailUrl'];
+  }
+
+  static List<ImageModel> fromJsonList(jsonList) {
+    return jsonList.map<ImageModel>((obj) => ImageModel.fromJson(obj)).toList();
+  }
+}
