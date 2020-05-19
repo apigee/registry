d1b4bfca92ce0a50d3fee64f44698ad73c965766
Tim Burks <timburks@google.com>
2020-05-18 19:44:08 -0700

    demo setup

---
+++ demo/SETUP.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cli flame update-project --project.name projects/atlas --project.display_name "Atlas" --project.description "from the APIs.guru OpenAPI Directory"
+
+cli flame update-project --project.name projects/google --project.display_name "Google APIs" --project.description "from the Google API Discovery Service"
