990666c6873c3450921441c4f9decfa322ab2f03
Tim Burks <timburks@google.com>
2020-07-01 20:16:57 -0700

    tests for resource name patterns

---
+++ models/models_test.go
@@ -0,0 +1,200 @@
+package models
+
+import (
+	"regexp"
+	"testing"
+)
+
+type group struct {
+	name   string
+	regexp *regexp.Regexp
+	pass   []string
+	fail   []string
+}
+
+func TestResourceNames(t *testing.T) {
+	{
+		groups := []group{
+			group{
+				name:   "projects",
+				regexp: ProjectsRegexp(),
+				pass: []string{
+					"projects",
+				},
+				fail: []string{
+					"-",
+					"project",
+					"organizations",
+					"products",
+				},
+			},
+			group{
+				name:   "project",
+				regexp: ProjectRegexp(),
+				pass: []string{
+					"projects/google",
+					"projects/-",
+				},
+				fail: []string{
+					"-",
+				},
+			},
+			group{
+				name:   "products",
+				regexp: ProductsRegexp(),
+				pass: []string{
+					"projects/google/products",
+					"projects/-/products",
+				},
+				fail: []string{
+					"-",
+				},
+			},
+			group{
+				name:   "product",
+				regexp: ProductRegexp(),
+				pass: []string{
+					"projects/google/products/sample",
+					"projects/-/products/-",
+					"projects/123/products/abc",
+					"projects/1-2_3/products/abc",
+				},
+				fail: []string{
+					"-",
+					"invalid",
+					"projects//products/123",
+					"projects/123/products/",
+					"projects/123/invalid/123",
+					"projects/123/products/ 123",
+				},
+			},
+			group{
+				name:   "versions",
+				regexp: VersionsRegexp(),
+				pass: []string{
+					"projects/google/products/sample/versions",
+					"projects/-/products/-/versions",
+				},
+				fail: []string{
+					"-",
+				},
+			},
+			group{
+				name:   "version",
+				regexp: VersionRegexp(),
+				pass: []string{
+					"projects/google/products/sample/versions/v1",
+					"projects/-/products/-/versions/-",
+					"projects/123/products/abc/versions/123",
+					"projects/1-2_3/products/abc/versions/123",
+				},
+				fail: []string{
+					"-",
+					"invalid",
+					"projects//products/123",
+					"projects/123/products/",
+					"projects/123/invalid/123",
+					"projects/123/products/ 123",
+				},
+			},
+			group{
+				name:   "specs",
+				regexp: SpecsRegexp(),
+				pass: []string{
+					"projects/google/products/sample/versions/v1/specs",
+					"projects/-/products/-/versions/-/specs",
+				},
+				fail: []string{
+					"-",
+				},
+			},
+			group{
+				name:   "spec",
+				regexp: SpecRegexp(),
+				pass: []string{
+					"projects/google/products/sample/versions/v1/specs/openapi.yaml@1234567890ABCDEFabcdef",
+					"projects/google/products/sample/versions/v1/specs/openapi.yaml",
+					"projects/-/products/-/versions/-/specs/-",
+					"projects/123/products/abc/versions/123/specs/abc",
+					"projects/1-2_3/products/abc/versions/123/specs/abc",
+				},
+				fail: []string{
+					"-",
+					"invalid",
+					"projects//products/123",
+					"projects/123/products/",
+					"projects/123/invalid/123",
+					"projects/123/products/ 123",
+				},
+			},
+			group{
+				name:   "properties",
+				regexp: PropertiesRegexp(),
+				pass: []string{
+					"projects/google/products/sample/versions/v1/specs/openapi.yaml/properties",
+					"projects/google/products/sample/versions/v1/properties",
+					"projects/google/products/sample/properties",
+					"projects/google/properties",
+				},
+				fail: []string{
+					"-",
+				},
+			},
+			group{
+				name:   "property",
+				regexp: PropertyRegexp(),
+				pass: []string{
+					"projects/google/products/sample/versions/v1/specs/openapi.yaml/properties/test-property",
+					"projects/google/products/sample/versions/v1/properties/test-property",
+					"projects/google/products/sample/properties/test-property",
+					"projects/google/properties/test-property",
+				},
+				fail: []string{
+					"-",
+				},
+			},
+			group{
+				name:   "labels",
+				regexp: LabelsRegexp(),
+				pass: []string{
+					"projects/google/products/sample/versions/v1/specs/openapi.yaml/labels",
+					"projects/google/products/sample/versions/v1/labels",
+					"projects/google/products/sample/labels",
+					"projects/google/labels",
+				},
+				fail: []string{
+					"-",
+				},
+			},
+			group{
+				name:   "label",
+				regexp: LabelRegexp(),
+				pass: []string{
+					"projects/google/products/sample/versions/v1/specs/openapi.yaml/labels/test-label",
+					"projects/google/products/sample/versions/v1/labels/test-label",
+					"projects/google/products/sample/labels/test-label",
+					"projects/google/labels/test-label",
+				},
+				fail: []string{
+					"-",
+				},
+			},
+		}
+		for _, g := range groups {
+			for _, path := range g.pass {
+				m := g.regexp.FindAllStringSubmatch(path, -1)
+				if m == nil {
+					t.Logf("failed to match %s: %s", g.name, path)
+					t.Fail()
+				}
+			}
+			for _, path := range g.fail {
+				m := g.regexp.FindAllStringSubmatch(path, -1)
+				if m != nil {
+					t.Logf("false match %s: %s", g.name, path)
+					t.Fail()
+				}
+			}
+		}
+	}
+}
