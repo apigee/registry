d55718c943c7e7e631eb87f853f672a4ad5003d1
Tim Burks <timburks@google.com>
2020-09-22 21:03:49 -0700

    Add license headers, remove unneeded datastore emulator configuration.

--- tools/EMULATOR-START.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# https://cloud.google.com/sdk/gcloud/reference/beta/emulators/datastore/start
-
-gcloud components install beta --quiet
-gcloud components install cloud-datastore-emulator --quiet
-gcloud beta emulators datastore start --no-store-on-disk &
-`gcloud beta emulators datastore env-init`
--- tools/EMULATOR-STOP.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-`gcloud beta emulators datastore env-unset`
-
-pgrep -f 'gcloud.py beta emulators datastore start' | xargs kill -9
