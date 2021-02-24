f6083187dd4ef254e9378efccfc2d635fd1c20e9
Tim Burks <timburks@google.com>
2021-02-24 13:27:23 -0800

    simplify map serialization to be non-deterministic

--- server/models/string_maps.go
+++ server/models/string_maps.go
@@ -15,32 +15,18 @@
 package models
 
 import (
-	"sort"
-
 	"github.com/apigee/registry/rpc"
 	"google.golang.org/protobuf/proto"
 )
 
-func bytesForMap(m map[string]string) ([]byte, error) {
-	entries := make([]*rpc.OrderedMap_Entry, 0)
-	for k, v := range m {
-		entries = append(entries, &rpc.OrderedMap_Entry{Name: k, Value: v})
-	}
-	sort.Slice(entries,
-		func(i, j int) bool {
-			return entries[i].Name < entries[j].Name
-		})
-	return proto.Marshal(&rpc.OrderedMap{Entries: entries})
+func bytesForMap(entries map[string]string) ([]byte, error) {
+	return proto.Marshal(&rpc.SerializableMap{Entries: entries})
 }
 
 func mapForBytes(b []byte) (map[string]string, error) {
-	om := &rpc.OrderedMap{}
-	if err := proto.Unmarshal(b, om); err != nil {
+	m := &rpc.SerializableMap{}
+	if err := proto.Unmarshal(b, m); err != nil {
 		return nil, err
 	}
-	m := make(map[string]string)
-	for _, p := range om.Entries {
-		m[p.Name] = p.Value
-	}
-	return m, nil
+	return m.Entries, nil
 }
