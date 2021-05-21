c24867b191570c088bcf3cdccd693cfa8bb2be2e
Tim Burks <timburks@google.com>
2021-05-21 09:26:17 -0700

    Add a Dockerfile for a container containing the registry CLI tools.

--- containers/BUILD.sh
+++ containers/BUILD.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2020 Google LLC. All Rights Reserved.
+# Copyright 2021 Google LLC. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,20 +20,23 @@
  
 # Available platforms will depend on the local installation of Docker.
 
-# ORGANIZATION should be dockerhub organization that will host the images
+# ORGANIZATION should be the dockerhub organization that will host the images
 #ORGANIZATION=
+
+# TARGET selects one of the container groups below and adds a suffix (if set)
+#TARGET=
 
 if [[ $TARGET == "dev" ]]
 then
   # If TARGET is specified as "dev", a minimal set of containers are built.
   # Container names have the suffix "-dev".
   SUFFIX="-$TARGET"
-  CONTAINERS=("registry-server" "authz-server")
+  CONTAINERS=("registry-server" "authz-server" "registry-tools")
   PLATFORMS="linux/arm64"
 else
   # Otherwise, all containers are built.
   SUFFIX=""
-  CONTAINERS=("registry-server" "authz-server" "registry-bundle")
+  CONTAINERS=("registry-server" "authz-server" "registry-tools" "registry-bundle")
   PLATFORMS="linux/amd64,linux/arm64"
 fi
 
