ef69b85ffe4476d21fa4cde427122b41e7d31a19
Sean Egan <egansean@google.com>
2021-07-26 17:41:11 -0500

    Use spf13/viper for server configuration

--- cmd/registry-server/main.go
+++ cmd/registry-server/main.go
@@ -16,50 +16,58 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net"
 	"os"
 	"os/signal"
-	"strconv"
+	"strings"
 	"syscall"
 
 	"github.com/apigee/registry/server"
-	"github.com/spf13/pflag"
-	"gopkg.in/yaml.v3"
+	"github.com/spf13/viper"
 )
 
+func init() {
+	// Enable environment variables.
+	// e.g. REGISTRY_DATABASE_DRIVER sets database.driver config value.
+	viper.SetEnvPrefix("registry")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
+	// Bind the REGISTRY_PROJECT_IDENTIFIER environment variable.
+	// For compatibility with other tools this value should be used if it's set.
+	viper.BindEnv("pubsub.project", "REGISTRY_PROJECT_IDENTIFIER")
+
+	// Enable config files.
+	viper.SetConfigName("registry-server")
+	viper.AddConfigPath("$HOME/.config/registry")
+	viper.AddConfigPath(".")
+}
+
 func main() {
-	var configPath string
-	pflag.StringVarP(&configPath, "config", "c", "", "specify a configuration file")
-	pflag.Parse()
-
-	var config server.Config
-	if configPath != "" {
-		if err := parseConfig(&config, configPath); err != nil {
-			log.Fatalf("Failed to parse config: %s", err)
-		} else if err := validateConfig(config); err != nil {
-			log.Fatalf("Invalid config: %s", err)
-		}
+	err := viper.ReadInConfig()
+	if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+		log.Println(err)
+	} else if err != nil {
+		log.Fatalf("Failed to read config: %s", err)
 	}
 
-	addr := &net.TCPAddr{Port: 8080}
-	if v, ok := os.LookupEnv("PORT"); ok {
-		if port, err := strconv.Atoi(v); err != nil {
-			log.Fatalf("Invalid $PORT %q: must be an integer", v)
-		} else {
-			addr.Port = port
-		}
-	}
-
-	listener, err := net.ListenTCP("tcp", addr)
+	listener, err := net.ListenTCP("tcp", &net.TCPAddr{
+		Port: viper.GetInt("port"),
+	})
 	if err != nil {
 		log.Fatalf("Failed to create TCP listener: %s", err)
 	}
 	defer listener.Close()
 
-	srv := server.New(config)
+	srv := server.New(server.Config{
+		Database:  viper.GetString("database.driver"),
+		DBConfig:  viper.GetString("database.dsn"),
+		Log:       viper.GetString("logging.level"),
+		Notify:    viper.GetBool("pubsub.enabled"),
+		ProjectID: viper.GetString("pubsub.project"),
+	})
+
 	go srv.Start(context.Background(), listener)
 	log.Printf("Listening on %s", listener.Addr())
 
@@ -67,56 +75,4 @@ func main() {
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGTERM)
 	<-done
-}
-
-func parseConfig(config *server.Config, filepath string) error {
-	fi, err := os.Lstat(filepath)
-	if err != nil {
-		return fmt.Errorf("failed to read file info: %s", err)
-	}
-
-	// Follow symbolic links to a readable config file if applicable.
-	if (fi.Mode() & os.ModeSymlink) != 0 {
-		target, err := os.Readlink(filepath)
-		if err != nil {
-			return fmt.Errorf("failed to read symbolic link %q: %s", filepath, err)
-		}
-		filepath = target
-	}
-
-	b, err := ioutil.ReadFile(filepath)
-	if err != nil {
-		return fmt.Errorf("failed to read file: %s", err)
-	}
-
-	b = []byte(os.ExpandEnv(string(b)))
-	if err := yaml.Unmarshal(b, &config); err != nil {
-		return fmt.Errorf("failed to unmarshal yaml: %s", err)
-	}
-
-	return nil
-}
-
-func validateConfig(c server.Config) error {
-	switch c.Database {
-	case "sqlite3", "postgres", "cloudsqlpostgres":
-	default:
-		return fmt.Errorf("invalid database value %q: must be one of [sqlite3, postgres, cloudsqlpostgres]", c.Database)
-	}
-
-	switch c.Log {
-	case "fatal", "error", "warn", "info", "debug":
-	default:
-		return fmt.Errorf("invalid log value %q: must be one of [fatal, error, warn, info, debug]", c.Log)
-	}
-
-	if c.DBConfig == "" {
-		return fmt.Errorf("invalid dbconfig %q: must not be empty", c.DBConfig)
-	}
-
-	if c.Notify && c.ProjectID == "" {
-		return fmt.Errorf("invalid project %q: notifications cannot be enabled without GCP project ID", c.ProjectID)
-	}
-
-	return nil
 }
