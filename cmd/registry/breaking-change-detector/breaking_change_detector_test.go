9b153e57525e8a20b2e6c6140547da0b5402e6bc
Jamaalbwells <43766242+Jamaalbwells@users.noreply.github.com>
2021-08-03 21:28:01 +0000

    Add Breaking Change Detector (#235)

---
+++ cmd/registry/breaking-change-detector/breaking_change_detector_test.go
@@ -0,0 +1,129 @@
+package breakingchangedetector
+
+import (
+	"testing"
+
+	"github.com/apigee/registry/rpc"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"google.golang.org/protobuf/testing/protocmp"
+)
+
+func TestChanges(t *testing.T) {
+	tests := []struct {
+		desc      string
+		diffProto *rpc.Diff
+		wantProto *rpc.ClassifiedChanges
+	}{
+		{
+			desc: "Components.Required field Addition Breaking Test",
+			diffProto: &rpc.Diff{
+				Additions: []string{"components.schemas.x.required.x"},
+			},
+			wantProto: &rpc.ClassifiedChanges{
+				BreakingChanges: &rpc.Diff{
+					Additions: []string{"components.schemas.x.required.x"},
+				},
+				NonBreakingChanges: &rpc.Diff{},
+				UnknownChanges:     &rpc.Diff{},
+			},
+		},
+		{
+			desc: "Components.Schemas field Deletion Breaking Test",
+			diffProto: &rpc.Diff{
+				Deletions: []string{"components.schemas.x.x"},
+			},
+			wantProto: &rpc.ClassifiedChanges{
+				BreakingChanges: &rpc.Diff{
+					Deletions: []string{"components.schemas.x.x"},
+				},
+				NonBreakingChanges: &rpc.Diff{},
+				UnknownChanges:     &rpc.Diff{},
+			},
+		},
+		{
+			desc: "Components.Schema.Type field Modification Breaking Test",
+			diffProto: &rpc.Diff{
+				Modifications: map[string]*rpc.Diff_ValueChange{
+					"components.schemas.x.properties.type": {
+						To:   "float",
+						From: "int64",
+					},
+				},
+			},
+			wantProto: &rpc.ClassifiedChanges{
+				BreakingChanges: &rpc.Diff{
+					Modifications: map[string]*rpc.Diff_ValueChange{
+						"components.schemas.x.properties.type": {
+							To:   "float",
+							From: "int64",
+						},
+					},
+				},
+				NonBreakingChanges: &rpc.Diff{},
+				UnknownChanges:     &rpc.Diff{},
+			},
+		},
+		{
+			desc: "Info field Addition NonBreaking Test",
+			diffProto: &rpc.Diff{
+				Additions: []string{"info.x.x"},
+			},
+			wantProto: &rpc.ClassifiedChanges{
+				BreakingChanges: &rpc.Diff{},
+				NonBreakingChanges: &rpc.Diff{
+					Additions: []string{"info.x.x"},
+				},
+				UnknownChanges: &rpc.Diff{},
+			},
+		},
+		{
+			desc: "Info field Deletion NonBreaking Test",
+			diffProto: &rpc.Diff{
+				Deletions: []string{"info.x.x"},
+			},
+			wantProto: &rpc.ClassifiedChanges{
+				BreakingChanges: &rpc.Diff{},
+				NonBreakingChanges: &rpc.Diff{
+					Deletions: []string{"info.x.x"},
+				},
+				UnknownChanges: &rpc.Diff{},
+			},
+		},
+		{
+			desc: "Info field Modification NonBreaking Test",
+			diffProto: &rpc.Diff{
+				Modifications: map[string]*rpc.Diff_ValueChange{
+					"info.x.x.x": {
+						To:   "to",
+						From: "from",
+					},
+				},
+			},
+			wantProto: &rpc.ClassifiedChanges{
+				BreakingChanges: &rpc.Diff{},
+				NonBreakingChanges: &rpc.Diff{
+					Modifications: map[string]*rpc.Diff_ValueChange{
+						"info.x.x.x": {
+							To:   "to",
+							From: "from",
+						},
+					},
+				},
+				UnknownChanges: &rpc.Diff{},
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			gotProto := GetClassifiedChanges(test.diffProto)
+			opts := cmp.Options{
+				protocmp.Transform(),
+				cmpopts.SortSlices(func(a, b string) bool { return a < b }),
+			}
+			if !cmp.Equal(test.wantProto, gotProto, opts) {
+				t.Errorf("GetDiff returned unexpected diff (-want +got):\n%s", cmp.Diff(test.wantProto, gotProto, opts))
+			}
+		})
+	}
+}
