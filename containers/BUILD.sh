acf7fdc05d4431d2aa17d3093e780561eebe305b
Tim Burks <timburks@google.com>
2021-10-05 17:48:42 -0700

    Remove authz-server (moved to registry-experimental) and update READMEs. (#327)
    Also adding deployments/envoy/proto.pb to support container builds from registry-experimental.

--- containers/BUILD.sh
+++ containers/BUILD.sh
@@ -20,23 +20,21 @@
  
 # Available platforms will depend on the local installation of Docker.
 
-# ORGANIZATION should be the dockerhub organization that will host the images
-#ORGANIZATION=
+# ORGANIZATION should be the dockerhub organization or gcr.io/project that will host the images
 
 # TARGET selects one of the container groups below and adds a suffix (if set)
-#TARGET=
 
 if [[ $TARGET == "dev" ]]
 then
   # If TARGET is specified as "dev", a minimal set of containers are built.
   # Container names have the suffix "-dev".
   SUFFIX="-$TARGET"
-  CONTAINERS=("registry-server" "authz-server" "registry-tools")
+  CONTAINERS=("registry-server" "registry-tools")
   PLATFORMS="linux/arm64"
 else
   # Otherwise, all containers are built.
   SUFFIX=""
-  CONTAINERS=("registry-server" "authz-server" "registry-tools" "registry-bundle")
+  CONTAINERS=("registry-server" "registry-tools")
   PLATFORMS="linux/amd64,linux/arm64"
 fi
 
--- containers/README.md
+++ containers/README.md
@@ -6,11 +6,5 @@ containers containing `registry-server` and related tools.
 Use `registry-server/Dockerfile` to build a container with `registry-server`
 only.
 
-Use `registry-bundle/Dockerfile` to build a container with `registry-server`,
-`envoy`, and `authz-server`. Envoy is configured to support grpc-web and to
-perform authorization using the `authz-server` that is included with this
-project, which is also included and run in the container.
-
-Use `authz-server/Dockerfile` to build a container with only `authz-server`.
-This can be deployed in Kubernetes in a three-container pod with
-`registry-server` and `envoy`.
+Use `registry-tools/Dockerfile` to build a container with `apg` and the
+`registry` tool.
