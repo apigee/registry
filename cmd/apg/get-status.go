798aec67ce4a4cff370225cd4fcc2367353cf57c
Sean Egan <egansean@google.com>
2021-10-13 14:35:08 -0600

    Add apg generated code to version control (#349)
    * Add apg generated code to version control
    * Regenerate RPCs

---
+++ cmd/apg/get-status.go
@@ -0,0 +1,41 @@
+// Code generated. DO NOT EDIT.
+
+package main
+
+import (
+	"github.com/spf13/cobra"
+
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
+
+	"fmt"
+)
+
+var GetStatusInput emptypb.Empty
+
+func init() {
+	RegistryServiceCmd.AddCommand(GetStatusCmd)
+
+}
+
+var GetStatusCmd = &cobra.Command{
+	Use:   "get-status",
+	Short: "GetStatus returns the status of the service. ...",
+	Long:  "GetStatus returns the status of the service.  GetStatus is for verifying open source deployments only  and is not included in hosted versions of the...",
+	PreRun: func(cmd *cobra.Command, args []string) {
+
+	},
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+
+		if Verbose {
+			printVerboseInput("Registry", "GetStatus", &GetStatusInput)
+		}
+		resp, err := RegistryClient.GetStatus(ctx, &GetStatusInput)
+
+		if Verbose {
+			fmt.Print("Output: ")
+		}
+		printMessage(resp)
+
+		return err
+	},
+}
--- go.mod
+++ go.mod
@@ -34,6 +34,19 @@ require (
 )
 
 require (
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.5 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/pelletier/go-toml v1.9.3 // indirect
+	github.com/spf13/afero v1.6.0 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	gopkg.in/ini.v1 v1.62.0 // indirect
+)
+
+require (
 	cloud.google.com/go v0.97.0 // indirect
 	github.com/antlr/antlr4/runtime/Go/antlr v0.0.0-20210930093333-01de314d7883 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
