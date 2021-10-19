1a007b1fd8e70a748c845669af85a1a07ce8bba4
Muhammad Haris <muhammadharis1010@gmail.com>
2021-10-19 13:37:08 -0400

    Implement a framework through which an arbitrary linter plugin can be created (#352)

---
+++ cmd/registry/plugins/linter/lint_framework.go
@@ -0,0 +1,67 @@
+package linter
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/apigee/registry/rpc"
+	"google.golang.org/protobuf/proto"
+)
+
+// RespondWithErrorAndExit takes in a sequence of errors, sets them in the response,
+// responds, and then exits.
+func respondWithErrorAndExit(errs ...error) {
+	errorMessages := make([]string, len(errs))
+	for i, err := range errs {
+		errorMessages[i] = err.Error()
+	}
+	response := &rpc.LinterResponse{
+		Errors: errorMessages,
+	}
+	respondAndExit(response)
+}
+
+// RespondAndExit serializes and writes the plugin response to STDOUT, and then exits.
+func respondAndExit(response *rpc.LinterResponse) {
+	responseBytes, _ := proto.Marshal(response)
+	os.Stdout.Write(responseBytes)
+	os.Exit(0)
+}
+
+// GetRequest constructs a LinterRequest object from standard input.
+func getRequest() (*rpc.LinterRequest, error) {
+	// Read from stdin.
+	pluginData, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(pluginData) == 0 && err != nil {
+		return nil, fmt.Errorf("no input data")
+	}
+
+	// Deserialize the request from the input into a request object.
+	linterRequest := &rpc.LinterRequest{}
+	if err := proto.Unmarshal(pluginData, linterRequest); err != nil {
+		return nil, err
+	}
+
+	return linterRequest, nil
+}
+
+// Lint reads the request from STDIN, runs the linter plugin, and
+// writes the response to STDOUT.
+func Lint(runner LinterRunner) {
+	req, err := getRequest()
+	if err != nil {
+		respondWithErrorAndExit(err)
+	}
+
+	resp, err := runner.Run(req)
+	if err != nil {
+		respondWithErrorAndExit(err)
+	}
+
+	respondAndExit(resp)
+}
