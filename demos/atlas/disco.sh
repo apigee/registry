f7f5569aa9386b4e6942655b2c1e6eb5304ae375
Tim Burks <timburks@google.com>
2021-10-26 10:26:45 -0700

    remove partial demos

--- demos/atlas/disco.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Upload all of the APIs from the Discovery Service at once.
-# This happens in parallel and usually takes a minute or two.
-registry upload bulk discovery \
-        --project_id atlas
-
