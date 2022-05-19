0dbef4a1f1a87451fd46818c385bbc092f197ddf
Tim Burks <timburks@google.com>
2022-05-19 15:46:43 -0700

    Simplify demo and walkthrough scripts, replace references to `apg` with `registry rpc` calls (#577)
    * Replace references to `apg` with `registry rpc` calls.
    * Fix small problems in walkthrough.sh
    * Update demos/README.md with info about setup-project.sh
    * Add --force flag to registry project deletion
    * Reduce demo scripts to basic API management.

--- demos/protos.sh
+++ demos/protos.sh
@@ -28,14 +28,7 @@
 # server, run `. auth/LOCAL.sh` in the shell before running the following
 # commands.
 
-# A registry exists under a top-level project.
-PROJECT=protos
-
-# First, delete and re-create the project to get a fresh start.
-apg admin delete-project --name projects/$PROJECT
-apg admin create-project --project_id $PROJECT \
-	--project.display_name "Google APIs" \
-	--project.description "Protocol buffer descriptions of public Google APIs"
+# This script assumes that PROJECT is set to the name of your registry project.
 
 # Get the commit hash of the checked-out protos directory
 export COMMIT=`(cd ~/Desktop/googleapis; git rev-parse HEAD)`
@@ -65,77 +58,23 @@ registry list projects/$PROJECT/locations/global/apis/-/versions/-/specs
 # To see more about an individual spec, use the `registry get` command:
 registry get projects/$PROJECT/locations/global/apis/translate/versions/v3/specs/google-cloud-translate-v3.zip
 
-# You can also get this with the automatically-generated `apg` command line tool:
-apg registry get-api-spec --name projects/$PROJECT/locations/global/apis/translate/versions/v3/specs/google-cloud-translate-v3.zip
+# You can also get this with direct calls to the registry rpc service:
+registry rpc get-api-spec --name projects/$PROJECT/locations/global/apis/translate/versions/v3/specs/google-cloud-translate-v3.zip
 
 # Add the `--json` flag to get this as JSON:
-apg registry get-api-spec --name projects/$PROJECT/locations/global/apis/translate/versions/v3/specs/google-cloud-translate-v3.zip --json
+registry rpc get-api-spec --name projects/$PROJECT/locations/global/apis/translate/versions/v3/specs/google-cloud-translate-v3.zip --json
 
 # You might notice that that didn't return the actual spec. That's because the spec contents
 # are accessed through a separate method that (when transcoded to HTTP) allows direct download
 # of spec contents.
-apg registry get-api-spec-contents --name projects/$PROJECT/locations/global/apis/translate/versions/v3/specs/google-cloud-translate-v3.zip
+registry rpc get-api-spec-contents --name projects/$PROJECT/locations/global/apis/translate/versions/v3/specs/google-cloud-translate-v3.zip > protos-1.zip
 
 # An easier way to get the bytes of the spec is to use `registry get` with the `--contents` flag.
 # This writes the bytes to stdout, so you probably want to redirect this to a file, as follows:
 registry get projects/$PROJECT/locations/global/apis/translate/versions/v3/specs/google-cloud-translate-v3.zip \
-	--contents > protos.zip
+	--contents > protos-2.zip
 
 # When you unzip this file, you'll find a directory hierarchy suitable for compiling with `protoc`.
 # protoc google/cloud/translate/v3/translation_service.proto -o.
 # (This requires additional protos that you can find in
 # [github.com/googleapis/api-common-protos](https://github.com/googleapis/api-common-protos).
-
-# The registry tool can compute simple complexity metrics for protos stored in the Registry.
-registry compute complexity projects/$PROJECT/locations/global/apis/-/versions/-/specs/-
-
-# Complexity results are stored in artifacts associated with the specs.
-registry list projects/$PROJECT/locations/global/apis/-/versions/-/specs/-/artifacts/complexity
-
-# We can use the `registry get` subcommand to read individual complexity records.
-registry get projects/$PROJECT/locations/global/apis/translate/versions/v3/specs/google-cloud-translate-v3.zip/artifacts/complexity
-
-# The registry tool also supports exporting all of the complexity results to a Google sheet.
-# (The following command expects OAuth client credentials with access to the
-# Google Sheets API to be available locally in ~/.credentials/registry.json)
-registry export sheet projects/$PROJECT/locations/global/apis/-/versions/-/specs/-/artifacts/complexity \
-	--as projects/$PROJECT/locations/global/artifacts/complexity-sheet
-
-# We can also compute the vocabulary of proto APIs.
-registry compute vocabulary projects/$PROJECT/locations/global/apis/-/versions/-/specs/-
-
-# Vocabularies are also stored as artifacts associated with API specs.
-registry get projects/$PROJECT/locations/global/apis/translate/versions/v3/specs/google-cloud-translate-v3.zip/artifacts/vocabulary
-
-# The registry command can perform set operations on vocabularies.
-# To find common terms in all Google speech-related APIs, use the following:
-registry vocabulary intersection projects/$PROJECT/locations/global/apis/-/versions/-/specs/-/artifacts/vocabulary \
-	--filter "api_id.contains('speech')"
-
-# We can also save this to a property.
-registry vocabulary intersection projects/$PROJECT/locations/global/apis/-/versions/-/specs/-/artifacts/vocabulary \
-	--filter "api_id.contains('speech')" --output projects/$PROJECT/locations/global/artifacts/speech-common
-
-# We can then read it directly or export it to a Google Sheet.
-registry get projects/$PROJECT/locations/global/artifacts/speech-common
-registry export sheet projects/$PROJECT/locations/global/artifacts/speech-common
-
-# To see a larger vocabulary, let's now compute the union of all the vocabularies in our project.
-registry vocabulary union projects/$PROJECT/locations/global/apis/-/versions/-/specs/-/artifacts/vocabulary \
-	--output projects/$PROJECT/locations/global/artifacts/vocabulary
-
-# We can also export this with `registry get` but it's easier to view this as a sheet:
-registry export sheet projects/$PROJECT/locations/global/artifacts/vocabulary
-
-# You'll notice that usage counts are included for each term, so we can sort by count
-# and find the most commonly-used terms across all of our APIs.
-# With vocabulary operations we can discover common terms across groups of APIs,
-# track changes across versions, and find unique terms in APIs that we are reviewing.
-# By storing these results and other artifacts in the Registry, we can build a
-# centralized store of API information that can help manage an API program.
-
-# We can also run analysis tools like linters and store the results in the Registry.
-# Here we run the Google api-linter and compile summary statistics.
-registry compute lint projects/$PROJECT/locations/global/apis/-/versions/-/specs/-
-registry compute lintstats projects/$PROJECT/locations/global/apis/-/versions/-/specs/- --linter aip
-registry compute lintstats projects/$PROJECT --linter aip
