5b9a711691d8a24c931cfd0777e4319859254026
shrutiparabgoogle <shrutiparab@google.com>
2022-05-23 15:31:39 -0700

    Reword flag description

--- cmd/registry/cmd/compute/compute.go
+++ cmd/registry/cmd/compute/compute.go
@@ -34,6 +34,6 @@ func Command() *cobra.Command {
 	cmd.AddCommand(vocabularyCommand())
 
 	cmd.PersistentFlags().String("filter", "", "Filter selected resources")
-	cmd.PersistentFlags().Bool("dry-run", false, "if set, computation results will only be printed and not uploaded to registry")
+	cmd.PersistentFlags().Bool("dry-run", false, "if set, computation results will only be printed and will not stored in the registry")
 	return cmd
 }
