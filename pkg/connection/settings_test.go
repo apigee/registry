abcb97c1f8b97258e7108fae3285e70e99b7bca0
Scott Ganyo <theganyo@google.com>
2022-07-12 14:24:30 -0700

    Load and use file-based configuration

---
+++ pkg/connection/settings_test.go
@@ -0,0 +1,183 @@
+// Copyright 2022 Google LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package connection
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/spf13/pflag"
+)
+
+func TestMain(m *testing.M) {
+	tmpDir, err := ioutil.TempDir("", "registry")
+	if err != nil {
+		panic("can't create temp dir")
+	}
+	configPath = tmpDir
+	code := m.Run()
+	os.Exit(code)
+}
+
+func TestSettingsActive(t *testing.T) {
+	// missing active file
+	settings, err := ActiveSettings()
+	if err == nil {
+		t.Errorf("expected error, got nil")
+	}
+
+	// missing config file
+	err = SetActiveConfigFile("missing")
+	if err != nil {
+		t.Fatal(err)
+	}
+	settings, err = ActiveSettings()
+	if err == nil {
+		t.Errorf("expected error, got nil")
+	}
+
+	// invalid config file
+	err = SetActiveConfigFile("invalid")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = Settings{}.Write("invalid")
+	if err != nil {
+		t.Fatal(err)
+	}
+	settings, err = ActiveSettings()
+	if err == nil {
+		t.Errorf("expected error, got nil")
+	}
+
+	// good config file
+	err = SetActiveConfigFile("good")
+	if err != nil {
+		t.Fatal(err)
+	}
+	settings = Settings{
+		Address:  "localhost:8080",
+		Insecure: true,
+		Token:    "unstored",
+	}
+	err = settings.Write("good")
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := ActiveSettings()
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+	settings.Token = "" // should not have been stored
+	if diff := cmp.Diff(settings, got); diff != "" {
+		t.Errorf("activeSettings returned unexpected diff: (-want +got):\n%s", diff)
+	}
+}
+
+func TestSettingsEnvVars(t *testing.T) {
+	// no config files
+	want := Settings{
+		Address:  "localhost:8080",
+		Insecure: true,
+		Token:    "mytoken",
+	}
+	os.Setenv("APG_REGISTRY_ADDRESS", want.Address)
+	defer os.Unsetenv("APG_REGISTRY_ADDRESS")
+	os.Setenv("APG_REGISTRY_INSECURE", strconv.FormatBool(want.Insecure))
+	defer os.Unsetenv("APG_REGISTRY_INSECURE")
+	os.Setenv("APG_REGISTRY_TOKEN", want.Token)
+	defer os.Unsetenv("APG_REGISTRY_TOKEN")
+
+	got, err := ActiveSettings()
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("activeSettings returned unexpected diff: (-want +got):\n%s", diff)
+	}
+
+	// good config file
+	err = SetActiveConfigFile("good")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = Settings{
+		Address: "overridden",
+		Token:   "overridden",
+	}.Write("good")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("activeSettings returned unexpected diff: (-want +got):\n%s", diff)
+	}
+}
+
+func TestSettingsDirectRead(t *testing.T) {
+	settings := Settings{
+		Address:  "localhost:8080",
+		Insecure: true,
+	}
+	err := settings.Write("good")
+	if err != nil {
+		t.Fatal(err)
+	}
+	configFile := filepath.Join(configPath, "good")
+
+	args := os.Args
+	defer func() { os.Args = args }()
+	os.Args = []string{
+		"test",
+		"--config", configFile,
+	}
+	pflag.CommandLine.AddFlagSet(Flags)
+	pflag.Parse()
+	got, err := ActiveSettings()
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+	if diff := cmp.Diff(settings, got); diff != "" {
+		t.Errorf("activeSettings returned unexpected diff: (-want +got):\n%s", diff)
+	}
+}
+
+func TestSettingsFlags(t *testing.T) {
+	want := Settings{
+		Address:  "localhost:8080",
+		Insecure: true,
+		Token:    "mytoken",
+	}
+	args := os.Args
+	defer func() { os.Args = args }()
+	os.Args = []string{
+		"test",
+		"--registry.address", want.Address,
+		"--registry.insecure", strconv.FormatBool(want.Insecure),
+		"--registry.token", want.Token,
+	}
+	pflag.CommandLine.AddFlagSet(Flags)
+	pflag.Parse()
+	got, err := ActiveSettings()
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("activeSettings returned unexpected diff: (-want +got):\n%s", diff)
+	}
+}
