a7a3a4495600618f89910de6023154610bd07902
Scott Ganyo <theganyo@google.com>
2022-07-14 17:13:47 -0700

    Enable config file usage and flags (#659)
    * Load and use file-based configuration
    
    * enable config file and flags in registry cmds
    
    * make generated CLIs use pkg/connection

--- cmd/registry/cmd/rpc/generated/provisioning_service.go
+++ cmd/registry/cmd/rpc/generated/provisioning_service.go
@@ -3,18 +3,12 @@
 package generated
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-	"golang.org/x/oauth2"
-	"google.golang.org/api/option"
-	"google.golang.org/grpc"
 
 	gapic "github.com/apigee/registry/gapic"
+	"github.com/apigee/registry/pkg/connection"
 )
 
-var ProvisioningConfig *viper.Viper
 var ProvisioningClient *gapic.ProvisioningClient
 var ProvisioningSubCommands []string = []string{
 	"create-instance",
@@ -25,10 +19,6 @@ var ProvisioningSubCommands []string = []string{
 func init() {
 	rootCmd.AddCommand(ProvisioningServiceCmd)
 
-	ProvisioningConfig = viper.New()
-	ProvisioningConfig.SetEnvPrefix("APG_REGISTRY")
-	ProvisioningConfig.AutomaticEnv()
-
 }
 
 var ProvisioningServiceCmd = &cobra.Command{
@@ -37,38 +27,7 @@ var ProvisioningServiceCmd = &cobra.Command{
 	Long:      "The service that is used for managing the data plane provisioning of the  Registry.",
 	ValidArgs: ProvisioningSubCommands,
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) (err error) {
-		var opts []option.ClientOption
-
-		address := ProvisioningConfig.GetString("address")
-		if address != "" {
-			opts = append(opts, option.WithEndpoint(address))
-		}
-
-		if ProvisioningConfig.GetBool("insecure") {
-			if address == "" {
-				return fmt.Errorf("Missing address to use with insecure connection")
-			}
-
-			conn, err := grpc.Dial(address, grpc.WithInsecure())
-			if err != nil {
-				return err
-			}
-			opts = append(opts, option.WithGRPCConn(conn))
-		}
-
-		if token := ProvisioningConfig.GetString("token"); token != "" {
-			opts = append(opts, option.WithTokenSource(oauth2.StaticTokenSource(
-				&oauth2.Token{
-					AccessToken: token,
-					TokenType:   "Bearer",
-				})))
-		}
-
-		if key := ProvisioningConfig.GetString("api_key"); key != "" {
-			opts = append(opts, option.WithAPIKey(key))
-		}
-
-		ProvisioningClient, err = gapic.NewProvisioningClient(ctx, opts...)
+		ProvisioningClient, err = connection.NewProvisioningClient(ctx)
 		return
 	},
 }
