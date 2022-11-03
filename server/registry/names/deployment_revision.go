844c6b9be2998f7cf08ea57285b44566690c700d
Scott Ganyo <theganyo@google.com>
2022-11-03 13:41:17 -0700

    artifacts on deployment revisions

--- server/registry/names/deployment_revision.go
+++ server/registry/names/deployment_revision.go
@@ -19,7 +19,11 @@ import (
 	"regexp"
 )
 
-var deploymentRevisionRegexp = regexp.MustCompile(fmt.Sprintf("^projects/%s/locations/%s/apis/%s/deployments/%s@%s$", identifier, Location, identifier, identifier, revisionTag))
+var deploymentRevisionRegexp = regexp.MustCompile(fmt.Sprintf("^projects/%s/locations/%s/apis/%s/deployments/%s(?:@%s)?$",
+	identifier, Location, identifier, identifier, revisionTag))
+
+var deploymentRevisionCollectionRegexp = regexp.MustCompile(fmt.Sprintf("^projects/%s/locations/%s/apis/%s/deployments/%s@",
+	identifier, Location, identifier, identifier))
 
 // DeploymentRevision represents a resource name for an API deployment revision.
 type DeploymentRevision struct {
@@ -38,9 +42,47 @@ func (s DeploymentRevision) Deployment() Deployment {
 	}
 }
 
+// Project returns the parent project for this resource.
+func (d DeploymentRevision) Project() Project {
+	return Project{
+		ProjectID: d.ProjectID,
+	}
+}
+
+// Api returns the parent API for this resource.
+func (d DeploymentRevision) Api() Api {
+	return Api{
+		ProjectID: d.ProjectID,
+		ApiID:     d.ApiID,
+	}
+}
+
+// Artifact returns an artifact with the provided ID and this resource as its parent.
+func (s DeploymentRevision) Artifact(id string) Artifact {
+	return Artifact{
+		name: deploymentArtifact{
+			ProjectID:    s.ProjectID,
+			ApiID:        s.ApiID,
+			DeploymentID: s.DeploymentID,
+			RevisionID:   s.RevisionID,
+			ArtifactID:   id,
+		},
+	}
+}
+
+// Parent returns this resource's parent API resource name.
+func (s DeploymentRevision) Parent() string {
+	return s.Deployment().Parent()
+}
+
 func (s DeploymentRevision) String() string {
-	return normalize(fmt.Sprintf("projects/%s/locations/%s/apis/%s/deployments/%s@%s",
-		s.ProjectID, Location, s.ApiID, s.DeploymentID, s.RevisionID))
+	if s.RevisionID == "" { // use latest revision
+		return normalize(fmt.Sprintf("projects/%s/locations/%s/apis/%s/deployments/%s",
+			s.ProjectID, Location, s.ApiID, s.DeploymentID))
+	} else {
+		return normalize(fmt.Sprintf("projects/%s/locations/%s/apis/%s/deployments/%s@%s",
+			s.ProjectID, Location, s.ApiID, s.DeploymentID, s.RevisionID))
+	}
 }
 
 // ParseDeploymentRevision parses the name of a deployment.
@@ -58,4 +100,19 @@ func ParseDeploymentRevision(name string) (DeploymentRevision, error) {
 	}
 
 	return revision, nil
+}
+
+func ParseDeploymentRevisionCollection(name string) (DeploymentRevision, error) {
+	r := deploymentRevisionCollectionRegexp
+	if !r.MatchString(name) {
+		return DeploymentRevision{}, fmt.Errorf("invalid deployment revision collection name %q: must match %q", name, r)
+	}
+
+	m := r.FindStringSubmatch(name)
+	return DeploymentRevision{
+		ProjectID:    m[1],
+		ApiID:        m[2],
+		DeploymentID: m[3],
+		RevisionID:   "-",
+	}, nil
 }
