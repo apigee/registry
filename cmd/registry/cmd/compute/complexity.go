4a9b8660710441c076f2faf2a74db422f29b245a
Tim Burks <timburks@google.com>
2022-11-04 12:50:40 -0700

    Add --jobs flag to registry compute subcommands. (#813)

--- cmd/registry/cmd/compute/complexity.go
+++ cmd/registry/cmd/compute/complexity.go
@@ -59,7 +59,11 @@ func complexityCommand() *cobra.Command {
 				log.FromContext(ctx).WithError(err).Fatal("Failed to get client")
 			}
 			// Initialize task queue.
-			taskQueue, wait := core.WorkerPool(ctx, 64)
+			jobs, err := cmd.Flags().GetInt("jobs")
+			if err != nil {
+				log.FromContext(ctx).WithError(err).Fatal("Failed to get jobs from flags")
+			}
+			taskQueue, wait := core.WorkerPool(ctx, jobs)
 			defer wait()
 
 			parsed, err := names.ParseSpecRevision(args[0])
