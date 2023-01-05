101d385d0742750229b933e528fe5c005ee7961e
Scott Ganyo <theganyo@google.com>
2023-01-05 13:49:29 -0800

    doc fix

--- cmd/registry/cmd/check/rules/rules.go
+++ cmd/registry/cmd/check/rules/rules.go
@@ -14,39 +14,21 @@
 
 // Package rules contains implementations of checker rules.
 //
-// Rules are sorted into subpackages by the AIP (https://aip.dev/) that
-// mandates the rule. Every rule represented in code here must be represented
-// in English in a corresponding AIP. Conversely, anything mandated in an AIP
-// should have a rule here if it is feasible to enforce in code (sometimes it
-// is infeasible, however).
+// Rules are sorted into groups. Every rule represented in code here must
+// be documented in English in a corresponding registry rule. Conversely,
+// anything mandated in such a rule should have a rule here if it is feasible
+// to enforce in code (sometimes it is infeasible, however).
 //
-// A rule is technically anything with a `GetName()`, `GetURI()“, and
-// `Lint(*desc.FileDescriptorProto) []lint.Problem` method, but most rule
-// authors will want to use the rule structs provided in the lint package
-// (`&lint.MessageRule`, `&lint.FieldRule`, and so on). These run against
-// each applicable descriptor in the file (`MessageRule` against every message,
-// for example). They also have an `OnlyIf` property that can be used to run
-// against a subset of descriptors.
-//
-// A simple rule therefore looks like this:
-//
-//	var myRule = &lint.MessageRule{
-//	  Name: lint.NewRuleName(1234, "my-rule"),
-//	  LintMessage: func(m *desc.MessageDescriptor) []lint.Problem {
-//	    if isBad(m) {
-//	      return []lint.Problem{{
-//	        Message: "This message is bad.",
-//	        Descriptor: m,
-//	      }}
-//	    }
-//	    return nil
-//	  },
-//	}
+// A rule is technically anything with a `GetName()` and `Apply(Resource)`
+// but most rule authors will want to use the rule structs provided in the
+// lint package (`lint.ProjectRule`, lint.FieldRule`, and so on). These run
+// against each applicable registry resource type or field. They also have
+// an `OnlyIf` property that can be used to run against a subset of resources.
 //
 // Once a rule is written, it needs to be registered. This involves adding
-// the rule to the `AddRules` method for the appropriate AIP package.
-// If this is the first rule for a new AIP, then the `rules.go` init() function
-// must also be updated to run the `AddRules` function for the new package.
+// the rule to the `AddRules` method for the appropriate group.
+// If this is the first rule for a new package, then the `rules.go` init() function
+// must also be updated to run the `Add` function for the new package.
 package rules
 
 import (
