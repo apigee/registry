8dd368a72ccd73defe4420975741d0447b00e89e
Scott Ganyo <theganyo@google.com>
2023-01-23 14:52:34 -0800

    add registry rules for ApiVersion

---
+++ cmd/registry/cmd/check/rules/rule103/rule103.go
@@ -0,0 +1,54 @@
+// Copyright 2023 Google LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Api state field is free-form, but we expect single words that describe state.
+// https://github.com/apigee/registry/blob/main/google/cloud/apigeeregistry/v1/registry_models.proto#L113
+package rule103
+
+import (
+	"context"
+	"strings"
+
+	"github.com/apigee/registry/cmd/registry/cmd/check/lint"
+	"github.com/apigee/registry/rpc"
+)
+
+var ruleNum = 103
+var ruleName = lint.NewRuleName(ruleNum, "version-state-single-word")
+
+// AddRules accepts a register function and registers each of
+// this rules' checks to the RuleRegistry.
+func AddRules(r lint.RuleRegistry) error {
+	return r.Register(
+		ruleNum,
+		stateSingleWord,
+	)
+}
+
+var stateSingleWord = &lint.ApiVersionRule{
+	Name: ruleName,
+	OnlyIf: func(a *rpc.ApiVersion) bool {
+		return true
+	},
+	ApplyToApiVersion: func(ctx context.Context, a *rpc.ApiVersion) []lint.Problem {
+		if arr := strings.SplitN(a.State, " ", 2); len(arr) > 1 {
+			return []lint.Problem{{
+				Severity:   lint.INFO,
+				Message:    `State is free-form, but we expect single words that describe API maturity.`,
+				Suggestion: `Use single words like: "CONCEPT", "DESIGN", "DEVELOPMENT", "STAGING", "PRODUCTION", "DEPRECATED", "RETIRED"`,
+			}}
+		}
+		return nil
+	},
+}
---
+++ cmd/registry/cmd/check/rules/rule104/rule104_test.go
@@ -0,0 +1,88 @@
+// Copyright 2023 Google LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rule104
+
+import (
+	"context"
+	"testing"
+
+	"github.com/apigee/registry/cmd/registry/cmd/check/lint"
+	"github.com/apigee/registry/pkg/connection/grpctest"
+	"github.com/apigee/registry/rpc"
+	"github.com/apigee/registry/server/registry"
+	"github.com/apigee/registry/server/registry/test/seeder"
+	"github.com/google/go-cmp/cmp"
+)
+
+// TestMain will set up a local RegistryServer and grpc.Server for all
+// tests in this package if APG_REGISTRY_ADDRESS env var is not set
+// for the client.
+func TestMain(m *testing.M) {
+	grpctest.TestMain(m, registry.Config{})
+}
+
+func TestAddRules(t *testing.T) {
+	if err := AddRules(lint.NewRuleRegistry()); err != nil {
+		t.Errorf("AddRules got an error: %v", err)
+	}
+}
+
+func Test_primarySpecRef(t *testing.T) {
+	ctx := context.Background()
+	registryClient, _ := grpctest.SetupRegistry(ctx, t, "check-test", []seeder.RegistryResource{
+		&rpc.ApiSpec{
+			Name: "projects/check-test/locations/global/apis/myapi/versions/myversion/specs/good",
+		},
+	})
+	ctx = context.WithValue(context.Background(), lint.ContextKeyRegistryClient, registryClient)
+
+	for _, tt := range []struct {
+		desc     string
+		in       string
+		expected []lint.Problem
+	}{
+		{"empty", "", nil},
+		{"unable to parse", "bad", []lint.Problem{{
+			Severity:   lint.ERROR,
+			Message:    `primary_spec "bad" is not a valid ApiSpec name.`,
+			Suggestion: `Parse error: invalid spec name "bad": must match "^projects/([A-Za-z0-9-.]+)/locations/global/apis/([A-Za-z0-9-.]+)/versions/([A-Za-z0-9-.]+)/specs/([A-Za-z0-9-.]+)$"`,
+		}}},
+		{"not a sibling", "projects/check-test/locations/global/apis/bad/versions/myversion/specs/bad", []lint.Problem{{
+			Severity:   lint.ERROR,
+			Message:    `primary_spec "projects/check-test/locations/global/apis/bad/versions/myversion/specs/bad" is not an API sibling of this Version.`,
+			Suggestion: `Correct the primary_spec.`,
+		}}},
+		{"missing", "projects/check-test/locations/global/apis/myapi/versions/myversion/specs/missing", []lint.Problem{{
+			Severity:   lint.ERROR,
+			Message:    `primary_spec "projects/check-test/locations/global/apis/myapi/versions/myversion/specs/missing" not found in registry.`,
+			Suggestion: `Correct the primary_spec.`,
+		}}},
+		{"good", "projects/check-test/locations/global/apis/myapi/versions/myversion/specs/good", nil},
+	} {
+		t.Run(tt.desc, func(t *testing.T) {
+			a := &rpc.ApiVersion{
+				Name:        "projects/check-test/locations/global/apis/myapi/versions/myversion",
+				PrimarySpec: tt.in,
+			}
+
+			if primarySpecRef.OnlyIf(a) {
+				got := primarySpecRef.ApplyToApiVersion(ctx, a)
+				if diff := cmp.Diff(got, tt.expected); diff != "" {
+					t.Errorf("unexpected diff: (-want +got):\n%s", diff)
+				}
+			}
+		})
+	}
+}
