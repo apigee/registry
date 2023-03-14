d9206f1f6a0ed406af496a24f3335c02389c0b13
Scott Ganyo <scott@ganyo.com>
2023-03-14 11:09:56 -0700

    fix name

--- cmd/registry/cmd/compute/score/score.go
+++ cmd/registry/cmd/compute/score/score.go
@@ -30,7 +30,7 @@ import (
 
 func Command() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "score RESOURCE",
+		Use:   "score PATTERN",
 		Short: "Compute scores for APIs and API specs",
 		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
