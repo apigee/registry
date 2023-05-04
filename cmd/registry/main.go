fceeaac20a03d51f8dafa93d528da7ba1123e766
Scott Ganyo <theganyo@google.com>
2023-05-04 16:39:43 -0700

    a basic take on plugins for registry

--- cmd/registry/main.go
+++ cmd/registry/main.go
@@ -17,12 +17,20 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"syscall"
 
 	"github.com/apigee/registry/cmd/registry/cmd"
 	"github.com/apigee/registry/pkg/log"
 	"github.com/google/uuid"
+	"github.com/spf13/cobra"
 )
+
+const pluginPrefix string = "registry-"
 
 func main() {
 	// Bind a logger instance to the local context with metadata for outbound requests.
@@ -32,10 +40,90 @@ func main() {
 	})
 
 	cmd := cmd.Command()
+	cmd.SetUsageTemplate(usageTemplate)
+	cobra.AddTemplateFunc("Plugins", plugins)
+
+	staySilent := cmd.SilenceErrors
+	cmd.SilenceErrors = true // don't print "unknown command" error
 	if err := cmd.ExecuteContext(ctx); err != nil {
-		fmt.Println("----")
-		fmt.Println("Need more help?")
-		fmt.Println("https://github.com/apigee/registry/wiki")
+		if strings.HasPrefix(err.Error(), "unknown command") && contains(plugins(), os.Args[1]) {
+			exCmd, err := exec.LookPath(pluginPrefix + os.Args[1])
+			if err == nil {
+				if err := syscall.Exec(exCmd, append([]string{exCmd}, os.Args[2:]...), os.Environ()); err != nil {
+					fmt.Fprintf(os.Stderr, "Command finished with error: %v", err)
+					os.Exit(127)
+				}
+			}
+		}
+		if !staySilent {
+			cmd.PrintErrln("Error:", err.Error())
+			cmd.PrintErrf("Run '%v --help' for usage.\n", cmd.CommandPath())
+		}
 		os.Exit(1)
 	}
+}
+
+// adds `Available Plugins` and `Need more help?` sections to default
+const usageTemplate = `Usage:{{if .Runnable}}
+{{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}
+{{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
+
+Aliases:
+{{.NameAndAliases}}{{end}}{{if .HasExample}}
+
+Examples:
+{{.Example}}{{end}}{{if .HasAvailableSubCommands}}
+
+Available Commands:{{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+{{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+
+Available Plugins:
+{{- range Plugins}}
+  {{.}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+
+Flags:
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
+
+Global Flags:
+{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
+
+Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+{{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+
+Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
+
+Need more help?
+https://github.com/apigee/registry/wiki
+`
+
+// list of executable files on path matching `registry-*
+func plugins() []string {
+	plugs := []string{}
+	paths := filepath.SplitList(os.Getenv("PATH"))
+	for _, path := range paths {
+		if files, err := ioutil.ReadDir(path); err == nil {
+			for _, file := range files {
+				if strings.HasPrefix(file.Name(), pluginPrefix) {
+					plug := strings.TrimPrefix(file.Name(), pluginPrefix)
+					if !contains(plugs, plug) {
+						if info, err := os.Stat(filepath.Join(path, file.Name())); err == nil {
+							if info.Mode()&0o111 != 0 {
+								plugs = append(plugs, plug)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return plugs
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
